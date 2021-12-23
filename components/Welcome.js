df4b7284a1b64c49a2521ac788027a158f57f31d
SoulReaper496 <praveendassking@gmail.com>
2021-12-23 20:16:03 +0530

    reactproject

---
+++ components/Welcome.js
@@ -0,0 +1,18 @@
+import react,{useState} from 'react';
+import { StyleSheet, Text, View,Button } from 'react-native';
+
+export default function Welcome() {
+    
+    const[name,setName] = useState('dass');
+
+    return(
+        <View style={styles.container}>
+          <Text>My name is {name}</Text>
+
+            </View>
+
+    )
+
+}
+
+
